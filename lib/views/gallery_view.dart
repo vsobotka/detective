ca0bda062729158897615ee501bc9294bbd0a962
Václav Sobotka <sobotka.vaclav@gmail.com>
2021-03-06 12:33:45 +0100

    Load gellery items individually for better fluidity

--- lib/views/gallery_view.dart
+++ lib/views/gallery_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pokeapi/model/pokemon/pokemon.dart';
 import 'package:pokeapi/pokeapi.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class GalleryView extends StatefulWidget {
   GalleryView({Key key, this.title}) : super(key: key);
@@ -12,8 +13,8 @@ class GalleryView extends StatefulWidget {
 
 class _GalleryViewState extends State<GalleryView> {
   ScrollController scrollController;
-  List<Pokemon> pokemonList;
   bool loading;
+  List<GalleryItem> pokemonList;
   static const crossAxisCount = 3;
   static const loadCount = 2 * crossAxisCount * crossAxisCount;
 
@@ -23,6 +24,7 @@ class _GalleryViewState extends State<GalleryView> {
     scrollController = new ScrollController()..addListener(_scrollListener);
     pokemonList = [];
     loading = false;
+    print(loadCount);
     _loadMorePokemon();
   }
 
@@ -40,20 +42,22 @@ class _GalleryViewState extends State<GalleryView> {
   }
 
   void _loadMorePokemon() {
-    _loadPokemon(pokemonList.length + 1, loadCount);
-  }
-
-  void _loadPokemon(int offset, int limit) {
     if (!loading) {
+      setState(() {
         loading = true;
-      PokeAPI.getObjectList<Pokemon>(offset, limit).then((poke) => {
-            setState(() => {pokemonList.addAll(poke), loading = false })
-          });
+        for (var i = 0; i < loadCount; i++) {
+          pokemonList.add(GalleryItem(
+            id: pokemonList.length + 1,
+          ));
+        }
+        loading = false;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
+    print(pokemonList.length);
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
@@ -61,29 +65,56 @@ class _GalleryViewState extends State<GalleryView> {
         body: GridView.builder(
             controller: scrollController,
             shrinkWrap: true,
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: crossAxisCount),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: crossAxisCount),
             itemCount: pokemonList.length,
             itemBuilder: (_, index) {
-              List<Widget> types = [];
-              var pokemonImg = Image.network(
-                  "https://pokeres.bastionbot.org/images/pokemon/${pokemonList[index].id}.png",
-                  fit: BoxFit.cover);
-              for (var i = 0; i < pokemonList[index].types.length; i++) {
-                var type = pokemonList[index].types[i].type.name;
-                types.add(Text(type));
-              }
-              return Stack(children: [
-                pokemonImg,
-                Column(children: types),
-                Align(
-                    alignment: FractionalOffset.bottomCenter,
-                    child: Text(pokemonList[index].name.toUpperCase(),
-                        style: TextStyle(
-                            height: 5,
-                            fontSize: 15,
-                            fontWeight: FontWeight.bold)))
-              ]);
+              return pokemonList[index];
             }));
+  }
+}
+
+class GalleryItem extends StatefulWidget {
+  GalleryItem({Key key, this.id}) : super(key: key);
+  final int id;
+
+  @override
+  _GalleryItemState createState() => _GalleryItemState();
+}
+
+class _GalleryItemState extends State<GalleryItem> {
+  Image image;
+  List<Text> types;
+  String name;
+
+  @override
+  void initState() {
+    super.initState();
+    types = [];
+    name = "";
+    PokeAPI.getObject<Pokemon>(widget.id).then((poke) => {
+          setState(() => {
+            poke.types.forEach((type) => {types.add(Text(type.type.name))}),
+            name = poke.name,
+          })
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      CachedNetworkImage(
+        imageUrl:
+            "https://pokeres.bastionbot.org/images/pokemon/${widget.id}.png",
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => new Icon(Icons.error),
+      ),
+      Column(children: types),
+      Align(
+          alignment: FractionalOffset.bottomCenter,
+          child: Text(name.toUpperCase(),
+              style: TextStyle(
+                  height: 5, fontSize: 15, fontWeight: FontWeight.bold)))
+    ]);
   }
 }
